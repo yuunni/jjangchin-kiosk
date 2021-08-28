f89e3f8a748fb723b7826e1bc124cd9dd373920c
jae073@naver.com <jae073@naver.com>
2021-08-28 21:39:40 +0900

    add kiosk button, add select page

---
+++ SelectPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Tizen.NUI;
+using Tizen.NUI.BaseComponents;
+
+namespace jjangchin_kiosk
+{
+	public partial class SelectPage : View
+    {
+        public SelectPage(string data)
+        {
+            InitializeComponent();
+
+            this.orderText.Text = $"[{data}]를 주문하시겠습니까?";
+        }
+    }
+}
