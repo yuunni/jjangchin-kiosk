404d56de851cdb6e7a4a033cf6ffbbf0e3e3af61
jae073@naver.com <jae073@naver.com>
2021-08-28 16:19:41 +0900

    add kioskpage_1

---
+++ kioskpage_1.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Tizen.NUI;
+using Tizen.NUI.BaseComponents;
+
+namespace jjangchin_kiosk
+{
+	public partial class kioskpage_1 : View
+{
+    public kioskpage_1()
+    {
+        InitializeComponent();
+    }
+}
+}
